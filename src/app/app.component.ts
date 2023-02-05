f70faead98e9e4d630c9513bd4116a1bccbc4b39
Masashi Yoshiwara <yoshiwara034@gmail.com>
2023-02-05 22:11:05 +0900

    css動的読み込みサンプル

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'dynamic-css-load';
+export class AppComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
+  cssBadePath: string = 'assets/css/';
+
+  ngOnInit(): void {
+    // css動的読み込み
+    of('sample.css').subscribe((res) => {
+      let cssLink: HTMLElement = this.renderer.createElement('link');
+      this.renderer.setAttribute(cssLink, 'rel', 'stylesheet');
+      this.renderer.setAttribute(cssLink, 'href', `${this.cssBadePath}${res}`);
+      this.renderer.appendChild(document.head, cssLink);
+    });
+  }
 }
